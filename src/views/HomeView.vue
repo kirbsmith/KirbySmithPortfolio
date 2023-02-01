eb5e468243225d454aa124b9b23a0a0ecbb20ac2
Kirby Smith <kirbsmith@gmail.com>
2023-02-01 17:20:48 -0500

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="bg-zinc-800 text-gray-200 px-2">
+    <div class="flex flex-col place-items-center pt-4 pb-8 font-montserrat">
+      <h1 class="text-3xl pb-4">Hi, I'm Kirby Smith</h1>
+      <img src="../assets/portfolioImage.jpg" alt="Picture of me" class="rounded-full h-64"/>
+      <h3 class="text-center font-light">Astronomy and engineering teacher turned front end developer</h3>
+      <p class="text-center py-4 font-light">After teaching for the past 7 years and completing a full-stack bootcamp, I have been working as a freelance developer with several clients in non-profit, construction, and retail fields</p>
+    </div>
+    <div class="flex flex-col place-items-center font-montserrat font-light gap-4 pb-4">
+      <a href="https://www.linkedin.com/in/kirbsmith/" target="_blank" class=" hover:bg-slate-600 hover:text-gray-300 duration-300 rounded-md px-2 py-1"><p>Let's connect on LinkedIn</p></a>
+      <a href="https://github.com/kirbsmith" target="_blank" class=" hover:bg-slate-600 hover:text-gray-300 duration-300 rounded-md px-2 py-1"><p>View my GitHub</p></a>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
 </script>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="text-center font-montserrat bg-zinc-800 text-gray-200">
+    <div class="pb
+    -6">
+      <a href="https://zengineschallengekirby.netlify.app/" target="_blank"><h2 class="py-4 hover:bg-slate-600 hover:text-gray-300 duration-300 mb-4 ">Hacker News Vue3 Clone</h2></a>
+      <p class="border-b-2 border-gray-800 pb-4 px-2">This Hacker News Clone takes the current top 50 stories and sorts them by popularity, displaying the story's title, author, score, comments, and posting date on the homescreen. The author screen displays the author's username, karma, total number of contributions (stories and comments), and a list of stories they have written with clickable links to view each. The comments page shows the story score, article ID, total comments, and a visible list of child comments. <strong>Tech used:</strong> Vue.js, Tailwind CSS, TypeScript</p>
+    </div>
+    <div class="pb-0">
+      <a href="https://capecodtelescope.netlify.app/" target="_blank"><h2 class="py-4 hover:bg-slate-600 hover:text-gray-300 duration-300 mb-4">Cape Cod Telescope</h2></a>
+      <p class="border-b-2 border-gray-800 pb-4 px-2">After teaching astronomy for the past few years, I realized there is a nearly insurmountable price barrier to students who want to take their own images of the night sky. This website allows users to search the NASA APOD by date for inspiration to request their own astrophotographs to be taken. It also includes an updated astronomical forecast for the location of the observatory and a contact form. <strong>Tech used:</strong> HTML, CSS, JavaScript, PHP</p>
+    </div>
+    <div class="pb-0">
+      <a href="https://mtgcards.netlify.app/" target="_blank"><h2 class="py-4 hover:bg-slate-600 hover:text-gray-300 duration-300 mb-4">Magic: The Gathering Card Game Player</h2></a>
+      <p class="border-b-2 border-gray-800 pb-4 px-2">During the earlier stages of the pandemic, when we were all at home, and my social group was spread across the country, I made this website to allow us to play a game and feel the social connection without being in person. Using the Magic: The Gathering API, this website allows you to keep track of cards in play while virtually playing the card game. Check out the <em>very, very much still a work in progress</em> new version<a href="https://magictestkirby.netlify.app" target="_blank" class=" hover:bg-slate-600 hover:text-gray-300 duration-300 rounded-md px-1">here</a>! <strong>Tech used:</strong> HTML, CSS, JavaScript</p>
+    </div>
+    <div class="pb-0">
+      <a href="https://plantid.netlify.app/" target="_blank"><h2 class="py-4 hover:bg-slate-600 hover:text-gray-300 duration-300 mb-4">Plant.id Website</h2></a>
+      <p class="border-b-2 border-gray-800 pb-4 px-2">After having difficulties identifying plants in my yard, I made this website. Plant.id API allows images to be sent and the plant species in those images to be identified with a confidence rating. A Wikipedia link is generated for each result if the user would like to do more research on their newly identified plant. <strong>Tech used:</strong> HTML, CSS, JavaScript</p>
+    </div>
+    <div class="pb-2">
+      <a href="https://thedreamblanket.netlify.app/" target="_blank"><h2 class="py-4 hover:bg-slate-600 hover:text-gray-300 duration-300 mb-4">The Dream Blanket</h2></a>
+      <p class="pb-4 px-2">The Dream Blanket is a non-profit book written to provide blankets and copies of the book to children living in shelters. This website was made for free in support of a good cause. <strong>Tech used:</strong> HTML, CSS</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+
+</script>
