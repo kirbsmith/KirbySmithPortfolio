bba2df2d37c1a8583abcc2856570a06bbe0c6e49
Kirby Smith <kirbsmith@gmail.com>
2023-02-12 06:27:14 -0500

    change homepage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="flex flex-row place-content-evenly py-4 bg-slate-500 text-zinc-100 font-light text-xl font-montserrat">
+  <nav class="flex flex-row place-content-evenly py-4 bg-slate-500 text-zinc-100 font-light text-xl font-montserrat z-50 h-16">
     <router-link to="/" class="px-4">Home</router-link> 
     <router-link to="/projects" class="px-4">Projects</router-link>
   </nav>
