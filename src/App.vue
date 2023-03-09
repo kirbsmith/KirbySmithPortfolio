490451d99ba1c5581d0e156e6ce79f0923598596
Kirby Smith <kirbsmith@gmail.com>
2023-03-09 09:45:46 -0500

    media queries and other updates

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <nav class="flex flex-col place-items-center py-4 bg-slate-500 text-zinc-100 font-light text-2xl font-montserrat hidden md:flex">
-    <div>
-      <router-link to="/" class="px-4">Home</router-link> 
+  <nav class="flex flex-col place-items-center py-4 bg-slate-500 text-zinc-200 font-light text-2xl font-montserrat hidden md:flex">
+    <div class="text-zinc-200">
+      <router-link to="/" class="px-4 text-zinc-200">Home</router-link> 
     </div>
     <div class="flex flex-row justify-center gap-8 text-zinc-800 pt-4">
-        <a href="https://www.linkedin.com/in/kirbsmith/" target="_blank"><font-awesome-icon icon="fa-brands fa-linkedin-in" class="hover:text-zinc-300 duration-300 border-b border-slate-900 pb-1"></font-awesome-icon></a>
-        <a href="https://github.com/kirbsmith" target="_blank"><font-awesome-icon icon="fa-brands fa-github" class="hover:text-zinc-300 duration-300 border-b border-slate-900 pb-1"></font-awesome-icon></a>
+        <a href="https://www.linkedin.com/in/kirbsmith/" target="_blank"><font-awesome-icon icon="fa-brands fa-linkedin-in" class="hover:text-zinc-300 duration-300 pb-1"></font-awesome-icon></a>
+        <a href="https://github.com/kirbsmith" target="_blank"><font-awesome-icon icon="fa-brands fa-github" class="hover:text-zinc-300 duration-300  pb-1"></font-awesome-icon></a>
     </div>
   </nav>
   <nav class="md:hidden bg-slate-500 text-zinc-100 font-light text-2xl flex flex-col">
@@ -17,11 +17,40 @@
       <router-link to="/" class="px-4 pb-2 border-b border-slate-700">Home</router-link> 
     </div>
     <div class="flex flex-row justify-center gap-8 mb-4 text-zinc-800">
-        <a href="https://www.linkedin.com/in/kirbsmith/" target="_blank"><font-awesome-icon icon="fa-brands fa-linkedin-in" class="hover:text-zinc-300 duration-300 border-b border-slate-900 pb-1"></font-awesome-icon></a>
-        <a href="https://github.com/kirbsmith" target="_blank"><font-awesome-icon icon="fa-brands fa-github" class="hover:text-zinc-300 duration-300 border-b border-slate-900 pb-1"></font-awesome-icon></a>
+        <a href="https://www.linkedin.com/in/kirbsmith/" target="_blank"><font-awesome-icon icon="fa-brands fa-linkedin-in" class="hover:text-zinc-300 duration-300 pb-1"></font-awesome-icon></a>
+        <a href="https://github.com/kirbsmith" target="_blank"><font-awesome-icon icon="fa-brands fa-github" class="hover:text-zinc-300 duration-300  pb-1"></font-awesome-icon></a>
     </div>
   </nav>
   <router-view/>
+  <div class="flex flex-col flex-wrap px-4 pt-2 bg-zinc-800 text-gray-200 font-thin">
+        <h2 class="text-2xl font-semibold border-b border-zinc-400 pb-4 mb-8">Latest Projects</h2>
+        <div class="md:flex md:flex-row md:gap-8 md:flex-wrap md:place-items-center md:place-content-center">
+            <div class="pb-12 border-b-2 border-zinc-500 mb-8 md:max-w-md">
+                <a href="https://zengineschallengekirby.netlify.app/" target="_blank"><img src="./assets/hackernews.png" alt="Screenshot of Hacker News Clone Website" class="rounded-xl"></a>
+                <h4 class="py-4 font-bold">Hacker News Clone</h4>
+                <p class="pb-12"><span class="font-bold">Vue 3, TypeScript, Node.js</span> - Clone of Hacker News that pulls the top 50 stories at the time of page load</p>
+                <a href="https://zengineschallengekirby.netlify.app/" target="_blank" ><button class="rounded-lg border-2 px-6 py-2 border-white">Visit</button></a>
+            </div>
+            <div class="pb-12 border-b-2 border-zinc-500 mb-8 md:max-w-md">
+                <a href="https://capecodtelescope.netlify.app/" target="_blank"><img src="./assets/capecodtelescope.png" alt="Screenshot of Cape Cod Telescope Website" class="rounded-xl"></a>
+                <h4 class="py-4 font-bold">Cape Cod Telescope</h4>
+                <p class="pb-12"><span class="font-bold">HTML, CSS, JavaScript, PHP</span> - Ever wanted to take your own astrophotos to show your friends? </p>
+                <a href="https://capecodtelescope.netlify.app/" target="_blank"><button class="rounded-lg border-2 px-6 py-2 border-white">Visit</button></a>
+            </div>
+            <div class="pb-12 border-b-2 border-zinc-500 mb-8 md:max-w-md">
+                <a href="https://mtgcards.netlify.app/" target="_blank"><img src="./assets/mtggame.png" alt="Screenshot of Magic: the Gathering Website" class="rounded-xl"></a>
+                <h4 class="py-4 font-bold">Magic: the Gathering Card Game</h4>
+                <p class="pb-12"><span class="font-bold">HTML, CSS, JavaScript</span> - Making playing MtG easier over zoom so you can track your opponent's cards</p>
+                <a href="https://mtgcards.netlify.app/" target="_blank"><button class="rounded-lg border-2 px-6 py-2 border-white">Visit</button></a>
+            </div>
+            <div class="pb-12 border-b-2 border-zinc-500 mb-8 md:max-w-md">
+                <a href="https://plantid.netlify.app/" target="_blank"><img src="./assets/plantid.png" alt="Screenshot of Plant Identification Website" class="rounded-xl duration-300"></a>
+                <h4 class="py-4 font-bold">Plant Identification</h4>
+                <p class="pb-12"><span class="font-bold">HTML, CSS, JavaScript</span> - You can upload images of plants and have them identified with a confidence rating</p>
+                <a href="https://plantid.netlify.app/" target="_blank"><button class="rounded-lg border-2 px-6 py-2 border-white">Visit</button></a>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
---
+++ src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="bg-zinc-800 text-gray-200 px-6 min-h-screen pt-6 font-thin md:flex md:flex-col place-content-center place-items-center">
+        <div class="md:flex md:flex-row-reverse md:place-content-center md:place-items-center md:gap-24">
+            <img src="../assets/portfolioImage.jpg" alt="Headshot image of me" class="w-5/6 md:w-1/2 rounded-xl my-8">
+            <h1 class="text-4xl font-thin">About Me</h1>
+        </div>
+        <p class="py-6">I love learning new things!</p>
+        <p class="py-2">I am always trying to think of different ways to solve problems. At the start of the pandemic, I designed, programmed, and built my own 3D printer and CNC. I spend my free time learning new code, doing house renovation projects, and trying my best not to shock myself running new wires.</p>
+        <p class="py-2">My name is Kirby Smith. I'm passionate about working positively with my colleagues to come to the best solutions to problems and challenges. I will do whatever it takes to learn more and hone my craft.</p>
+        <p class="py-2">Get in touch to find solutions together!</p>
+        <div class="border-b pb-4">
+            <a :href="`${publicPath}resume.pdf`" download="Kirby Smith Resume"><button class="border-2 border-zinc-200 rounded-lg px-4 py-2 my-4">Download resume</button></a>
+        </div>
+        <div class="pt-6">
+            <div>
+                <h3 class="text-lg underline pb-3 font-light">Web Development</h3>
+                <p class="pb-4 font-thin">Front-end and back-end web development through the full lifecycle of a project</p>
+            </div>
+            <div>
+                <h3 class="text-lg underline pb-3 font-light">UI/UX Design</h3>
+                <p class="pb-4 font-thin">Functional and efficient design decisions that create easy-to-navigate websites that follow accessibility rules and guidelines</p>
+            </div>
+            <div>
+                <h3 class="text-lg underline pb-3 font-light">Responsive Design</h3>
+                <p class="pb-4 font-thin">Websites that run beautifully on all devices and platforms with functionally aesthetic page structure</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+let publicPath = process.env.BASE_URL
+
+</script>
